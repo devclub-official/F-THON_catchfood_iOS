e933d61101355b4c6e9b4cc267d1d4d1fc02206e
Tori <ttang1135@naver.com>
2025-04-19 06:51:27 +0900

    feat: 멤버 초대

--- catchfood/catchfood/GroupMemberInviteViewController.swift
+++ catchfood/catchfood/GroupMemberInviteViewController.swift
@@ -5,33 +5,113 @@
 //  Created by 정종찬 on 4/19/25.
 //
 
-import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 import SnapKit
 
-class GroupMemberInviteViewController: UIViewController {
+final class GroupMemberInviteViewController: UIViewController {
+    private let memberTF = CFTextField(placeholder: "초대할 닉네임을 적어주세요")
+    private let inviteButton = CFButton(styleType: .outline, title: "초대")
     
-    private var containerView : UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .white
-        return view
+    private let memberListLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        label.textColor = .aTeamHeadLineTextColor
+        label.text = "멤버 목록"
+        return label
     }()
     
+    private let tableView = UITableView()
+    
+    private let viewModel = GroupMemberInviteViewModel()
+    private let disposeBag = DisposeBag()
+    private let inviteTrigger = PublishRelay<String>()
+    private var members: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setViewLayout()
+        setupTableView()
         bind()
     }
     
-    func setViewLayout()
-    {
+    private func setViewLayout() {
+        view.backgroundColor = .white
+        view.addSubviews(views: [
+            memberTF,
+            inviteButton,
+            memberListLabel,
+            tableView
+        ])
         
+        memberTF.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        inviteButton.snp.makeConstraints { make in
+            make.top.equalTo(memberTF.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        memberListLabel.snp.makeConstraints { make in
+            make.top.equalTo(inviteButton.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(memberListLabel.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
+    }
+
+    private func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+    }
+
+    private func bind() {
+        // 초대 버튼 탭 시 inviteTrigger로 닉네임 전달
+        inviteButton.rx.tap
+            .withUnretained(self)
+            .compactMap { vc, _ in
+                vc.memberTF.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+            }
+            .filter { !$0.isEmpty }
+            .bind(to: inviteTrigger)
+            .disposed(by: disposeBag)
+        
+        // ViewModel transform
+        let input = GroupMemberInviteViewModel.Input(inviteMember: inviteTrigger.asObservable())
+        let output = viewModel.transform(input: input)
+        
+        // 멤버 리스트 바인딩
+        output.currentMember
+            .drive(onNext: { [weak self] memberList in
+                print(memberList)
+                self?.members = memberList
+                self?.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
+        
+        output.inviteSuccess
+            .emit(onNext: { [weak self] in
+                self?.memberTF.text = ""
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension GroupMemberInviteViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return members.count
     }
     
-    func bind()
-    {
-        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = members[indexPath.row]
+        return cell
     }
 }
---
+++ catchfood/catchfood/GroupMemberInviteViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  GroupMemberInviteViewModel.swift
+//  catchfood
+//
+//  Created by 방유빈 on 4/19/25.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+struct GroupMemberDTO: Codable {
+    let data: [String]
+}
+final class GroupMemberInviteViewModel {
+    struct Input {
+        let inviteMember: Observable<String>
+    }
+    
+    struct Output {
+        let currentMember: Driver<[String]>
+        let inviteSuccess: Signal<Void>
+    }
+    func transform(input: Input) -> Output {
+        let id = "3"
+
+        let initialMembers = APIService.shared.request(
+            APIEndpoint(
+                path: "/parties/\(id)/members",
+                method: .get,
+                headers: ["X-User-Name": NicknameStorageService.shared.getNickname() ?? "Yubin"]
+            ),
+            type: GroupMemberDTO.self
+        )
+        .map { $0.data }
+        .catchAndReturn([])
+        .share(replay: 1)
+
+        // 초대 API 요청 처리
+        let invitedMembers = input.inviteMember
+            .flatMapLatest { newMember -> Observable<String> in
+                let endpoint = APIEndpoint(
+                    path: "/parties/\(id)/members",
+                    method: .post,
+                    parameters: ["memberName": newMember],
+                    headers: ["X-User-Name": NicknameStorageService.shared.getNickname() ?? ""]
+                )
+
+                return APIService.shared.request(endpoint, type: StatusResponse.self)
+                    .map { $0.status == "SUCCESS" ? newMember : "" }
+                    .catchAndReturn("")
+            }
+            .filter { !$0.isEmpty }
+
+        // 멤버 누적
+        let allMembers = Observable
+            .combineLatest(initialMembers, invitedMembers.scan([]) { acc, new in acc + [new] }.startWith([]))
+            .map { initial, added in initial + added }
+
+        let inviteSuccess = invitedMembers
+            .map { _ in () } // 성공한 초대만 emit
+            .asSignal(onErrorSignalWith: .empty())
+
+        return Output(
+            currentMember: allMembers.asDriver(onErrorJustReturn: []),
+            inviteSuccess: inviteSuccess
+        )
+    }
+}
+
