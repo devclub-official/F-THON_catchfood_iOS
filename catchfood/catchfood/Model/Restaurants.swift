5be0f3a00c45cc6df94d41569c90296ca86a82d7
Tori <ttang1135@naver.com>
2025-04-19 02:59:26 +0900

    feat: Search List -> 음식점 디테일뷰 연결

--- catchfood/catchfood/Model/Restaurants.swift
+++ catchfood/catchfood/Model/Restaurants.swift
@@ -6,6 +6,7 @@
 //
 
 struct Restaurants {
+    let id: Int
     let imageStr: String
     let title: String
     let category: String
@@ -13,8 +14,8 @@ struct Restaurants {
     let isLike: Bool
     
     static let dummyData: [Restaurants] = [
-        Restaurants(imageStr: "https://lh6.googleusercontent.com/proxy/NFPYLgyvDjVdVa7o232ikTC8VYaT6slShCtl70COmlngcXsn_nJfIaLiSKBmhykZK3osVYYauCTM48Hoc6fyNnq09E4P32t6WI6U0XzqMKU", title: "짱끝내주는 음식점", category: "한식", minutes: 10, isLike: true),
-        Restaurants(imageStr: "https://lh6.googleusercontent.com/proxy/NFPYLgyvDjVdVa7o232ikTC8VYaT6slShCtl70COmlngcXsn_nJfIaLiSKBmhykZK3osVYYauCTM48Hoc6fyNnq09E4P32t6WI6U0XzqMKU", title: "짱끝내주는 음식점2", category: "양식", minutes: 5, isLike: false),
-        Restaurants(imageStr: "https://lh6.googleusercontent.com/proxy/NFPYLgyvDjVdVa7o232ikTC8VYaT6slShCtl70COmlngcXsn_nJfIaLiSKBmhykZK3osVYYauCTM48Hoc6fyNnq09E4P32t6WI6U0XzqMKU", title: "짱끝내주는 음식점3", category: "중식", minutes: 15, isLike: false),
+        Restaurants(id: 1, imageStr: "https://lh6.googleusercontent.com/proxy/NFPYLgyvDjVdVa7o232ikTC8VYaT6slShCtl70COmlngcXsn_nJfIaLiSKBmhykZK3osVYYauCTM48Hoc6fyNnq09E4P32t6WI6U0XzqMKU", title: "짱끝내주는 음식점", category: "한식", minutes: 10, isLike: true),
+        Restaurants(id: 2, imageStr: "https://lh6.googleusercontent.com/proxy/NFPYLgyvDjVdVa7o232ikTC8VYaT6slShCtl70COmlngcXsn_nJfIaLiSKBmhykZK3osVYYauCTM48Hoc6fyNnq09E4P32t6WI6U0XzqMKU", title: "짱끝내주는 음식점2", category: "양식", minutes: 5, isLike: false),
+        Restaurants(id: 3, imageStr: "https://lh6.googleusercontent.com/proxy/NFPYLgyvDjVdVa7o232ikTC8VYaT6slShCtl70COmlngcXsn_nJfIaLiSKBmhykZK3osVYYauCTM48Hoc6fyNnq09E4P32t6WI6U0XzqMKU", title: "짱끝내주는 음식점3", category: "중식", minutes: 15, isLike: false),
     ]
 }
--- catchfood/catchfood/SceneDelegate.swift
+++ catchfood/catchfood/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let viewController = RestaurantDetailViewController(restaurantId: 1)
+        let viewController = UINavigationController(rootViewController: SearchViewController())
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
