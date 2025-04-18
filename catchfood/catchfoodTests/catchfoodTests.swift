b9df234f292284bb26eae60426abd55f6e0b4e2e
JJC <qppl950@gmail.com>
2025-04-18 22:26:27 +0900

    init

---
+++ catchfood/catchfoodTests/catchfoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  catchfoodTests.swift
+//  catchfoodTests
+//
+//  Created by 정종찬 on 4/18/25.
+//
+
+import Testing
+@testable import catchfood
+
+struct catchfoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
